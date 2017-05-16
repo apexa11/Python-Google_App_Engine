6b583590228fb39613bf7db7d2e9aad48a112f63
apexa11 <apexachoxi7@gmail.com>
2017-05-16 13:21:28 -0400

    if-else statment

--- main.py
+++ main.py
@@ -50,7 +50,10 @@ class Handler(webapp2.RequestHandler):
 
 class MainHandler(Handler):
     def get(self):
-        self.render("shopping_list.html",name=self.request.get("name"))
+        n=self.request.get("n")
+        if n:
+            n = int(n)
+        self.render("shopping_list.html", n=n)
 
 
         #output=form_html
